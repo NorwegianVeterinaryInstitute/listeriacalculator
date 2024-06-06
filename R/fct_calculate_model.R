76197e29f70860548abfd1a4351151f4c1d81a13
novica <nnovica@gmail.com>
2024-06-06 12:10:18 +0200

    fix: reference line stays fixed.

--- R/fct_calculate_model.R
+++ R/fct_calculate_model.R
@@ -11,15 +11,21 @@
 #' It returns values that are equivalent to row 1 and row 2 of
 #' the results table (A54) BUT without the cumulative sum.
 #'
+#' Reference data points are always calculated with default values.
+#' For the first mini table those are temperature = 4 and days = 3.
+#'
 #' @return a list of calculated data
 #'
 #' @noRd
 calc_mini_table_1 <- function(prod_temp = 4,
                               prod_days = 3,
-                              initial_conc = 0.2) {
+                              initial_conc = 1) {
   log_ic <- log10(as.numeric(initial_conc))
 
   ref_growth <- 0.007343
+  ref_temp <- 4
+  ref_days <- 3
+
   likely_growth <- ref_growth * (prod_temp + 1.5) ^ 2 / (prod_temp + 1.5) ^ 2
   row_1 <- c(
     reference = 0,
@@ -28,7 +34,7 @@ calc_mini_table_1 <- function(prod_temp = 4,
     max = log_ic
   )
   row_2 <- c(
-    reference = ref_growth * prod_days * 24,
+    reference = ref_growth * ref_days * 24,
     likely = likely_growth * prod_days * 24,
     min = 0.9 * likely_growth * prod_days * 24,
     max = 1.1 * likely_growth * prod_days * 24
@@ -43,7 +49,6 @@ calc_mini_table_1 <- function(prod_temp = 4,
 #'
 #' @param store_temp temperature in Celsius
 #' @param store_days period in days
-#' @param initial_conc initial concentration of bacteria
 #'
 #' @details
 #' This function calculates the values that are seen in the
@@ -52,18 +57,22 @@ calc_mini_table_1 <- function(prod_temp = 4,
 #' It returns values that are equivalent to row 3 of
 #' the results table (A54) BUT without the cumulative sum.
 #'
+#' Reference data points are always calculated with default values.
+#' For the second mini table those are temperature = 4 and days = 3.
+#'
 #' @return a list of calculated data
 #'
 #' @noRd
 calc_mini_table_2 <- function(store_temp = 4,
-                              store_days = 3,
-                              initial_conc = 0.2) {
-  log_ic <- log10(as.numeric(initial_conc))
+                              store_days = 3) {
+
 
   ref_growth <- 0.007343
+  ref_temp <- 4
+  ref_days <- 3
   likely_growth <- ref_growth * (store_temp + 1.5) ^ 2 / (store_temp + 1.5) ^ 2
   row_3 <- c(
-    reference = ref_growth * store_days * 24,
+    reference = ref_growth * ref_days * 24,
     likely = likely_growth * store_days * 24,
     min = 0.9 * likely_growth * store_days * 24,
     max = 1.1 * likely_growth * store_days * 24
@@ -81,7 +90,6 @@ calc_mini_table_2 <- function(store_temp = 4,
 #' @param home_hours period in hours
 #' @param salmon_temp temperature in Celsius
 #' @param salmon_hours period in hours
-#' @param initial_conc initial concentration of bacteria
 #'
 #' @details
 #' This function calculates the values that are seen in the
@@ -94,6 +102,10 @@ calc_mini_table_2 <- function(store_temp = 4,
 #' We do the two mini tables together because the reference growth for
 #' the next step depends on the one in the previous step.
 #'
+#' Reference data points are always calculated with default values.
+#' For the third and fourth mini table those are
+#' temperature = 10 and hours = 3.
+#'
 #' @return a list of calculated data
 #'
 #' @noRd
@@ -101,28 +113,30 @@ calc_mini_table_3_4 <- function(prod_temp = 4,
                                 home_temp = 10,
                                 home_hours = 3,
                                 salmon_temp = 10,
-                                salmon_hours = 3,
-                                initial_conc = 0.2) {
-  log_ic <- log10(as.numeric(initial_conc))
+                                salmon_hours = 3) {
 
-  ref_growth <- 0.007343 * (home_temp + 1.5) ^ 2 / (prod_temp + 1.5) ^ 2
+  ref_growth <- 0.007343 * (10 + 1.5) ^ 2 / (4 + 1.5) ^ 2
+  ref_temp <- 10
+  ref_hours <- 3
 
   likely_growth <- ref_growth
 
   row_4 <- c(
-    reference = ref_growth * home_hours,
+    reference = ref_growth * ref_hours,
     likely = likely_growth * home_hours,
     min = 0.9 * likely_growth * home_hours,
     max = 1.1 * likely_growth * home_hours
   )
 
 
-  ref_growth_2 <- ref_growth * (salmon_temp + 1.5) ^ 2 / (home_temp + 1.5) ^ 2
+  ref_growth_2 <- ref_growth * (10 + 1.5) ^ 2 / (10 + 1.5) ^ 2
+  ref_temp_2 <- 10
+  ref_hours_2 <- 3
 
   likely_growth_2 <- ref_growth_2
 
   row_5 <- c(
-    reference = ref_growth_2 * salmon_hours,
+    reference = ref_growth_2 * ref_hours_2,
     likely = likely_growth_2 * salmon_hours,
     min = 0.9 * likely_growth * salmon_hours,
     max = 1.1 * likely_growth * salmon_hours
@@ -139,7 +153,6 @@ calc_mini_table_3_4 <- function(prod_temp = 4,
 #' @param sushi_hours period in hours
 #' @param period_temp temperature in Celsius
 #' @param period_hours period in hours
-#' @param initial_conc initial concentration of bacteria
 #'
 #' @details
 #' This function calculates the values that are seen in the
@@ -150,32 +163,39 @@ calc_mini_table_3_4 <- function(prod_temp = 4,
 #' It returns values that are equivalent to row 1 and row 2 of
 #' the results table (A54) BUT without the cumulative sum.
 #'
+#' Reference data points are always calculated with default values.
+#' For the fifth and sixth mini table those are
+#' temperature = 4 and hours = 12, and temperature = 22 and hours 6
+#' respectively.
+#'
 #' @return a list of calculated data
 #'
 #' @noRd
 calc_mini_table_5_6 <- function(sushi_temp = 4,
                                 sushi_hours = 12,
                                 period_temp = 22,
-                                period_hours = 6,
-                                initial_conc = 0.2) {
-  log_ic <- log10(as.numeric(initial_conc))
+                                period_hours = 6) {
 
   ref_growth <- 0.00286
+  ref_temp <- 4
+  ref_hours <- 12
   likely_growth <- ref_growth * (sushi_temp + 1.5) ^ 2 / (sushi_temp + 1.5) ^ 2
   row_6 <- c(
-    reference = ref_growth * sushi_hours,
+    reference = ref_growth * ref_temp,
     likely = likely_growth * sushi_hours,
     min = 0.9 * likely_growth * sushi_hours,
     max = 1.1 * likely_growth * sushi_hours
   )
 
   ref_growth_2 <- 0.06
+  ref_temp_2 <- 22
+  ref_hours_2 <- 6
   likely_growth_2 <- ref_growth_2 * (period_temp + 1.5) ^ 2 / (period_temp + 1.5) ^ 2
   row_7 <- c(
-    reference = ref_growth * period_hours,
-    likely = likely_growth * period_hours,
-    min = 0.9 * likely_growth * period_hours,
-    max = 1.1 * likely_growth * period_hours
+    reference = ref_growth_2 * ref_temp_2,
+    likely = likely_growth_2 * period_hours,
+    min = 0.9 * likely_growth_2 * period_hours,
+    max = 1.1 * likely_growth_2 * period_hours
   )
 
   return(list(sushi = row_6, period = row_7))
@@ -208,28 +228,27 @@ calc_wrapper <- function(prod_temp = 4,
                          period_temp = 22,
                          period_hours = 6,
                          initial_conc = 1) {
-  dat_1 <- calc_mini_table_1(prod_temp = prod_temp,
-                             prod_days = prod_days,
-                             initial_conc = initial_conc)
+  dat_1 <- calc_mini_table_1(
+    prod_temp = prod_temp,
+    prod_days = prod_days,
+    initial_conc = initial_conc
+  )
   dat_2 <- calc_mini_table_2(
     store_temp = store_temp,
-    store_days = store_days,
-    initial_conc = initial_conc
+    store_days = store_days
   )
   dat_3 <- calc_mini_table_3_4(
     prod_temp = prod_temp,
     home_temp = home_temp,
     home_hours = home_hours,
     salmon_temp = salmon_temp,
-    salmon_hours = salmon_hours,
-    initial_conc = initial_conc
+    salmon_hours = salmon_hours
   )
   dat_4 <- calc_mini_table_5_6(
     sushi_temp = sushi_temp,
     sushi_hours = sushi_hours,
     period_temp = period_temp,
-    period_hours = period_hours,
-    initial_conc = initial_conc
+    period_hours = period_hours
   )
   dat <- data.frame(rbind(
     t(data.frame(dat_1)),
