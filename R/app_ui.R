1650a61aa64d5571d6ddd55a12db3fd186296a1c
novica <nnovica@gmail.com>
2025-05-12 11:13:41 +0200

    set html tag for language

--- R/app_ui.R
+++ R/app_ui.R
@@ -74,6 +74,7 @@ golem_add_external_resources <- function() {
 
   tags$head(
     favicon(ext = "png"),
+    tags$html(lang = "en"),
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "listeriacalculator"
